d4c67df4a8c61ff003c4a18340c5c23fa0fe6f6e
destinr <destinroyer@gmail.com>
2022-11-08 14:11:49 -0800

    Meets amazon spec.

--- amazon/store/models.py
+++ amazon/store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    pass
+
+class Shop(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name="owner")
+    
+class Product(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name = "products")
+        
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name = 'reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+
